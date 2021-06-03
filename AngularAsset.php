ff91b5eaef1dd1bd8f8bbc0d60879023e981704e
Saleem Ahmed <salimkamboh@gmail.com>
2021-06-03 13:35:12 +0500

    Adding tesjin repo files

---
+++ AngularAsset.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @link http://www.tesjin.ru/
+ * @copyright Copyright (c) 2014 TesJin Group
+ * @license BSD-3-Clause
+ */
+
+namespace yii\angularjs;
+
+use yii\web\AssetBundle;
+
+/**
+ * This asset bundle provides the [angular javascript library](https://angularjs.org/)
+ *
+ * @author Vladislav Orlov <orlov@tesjin.ru>
+ */
+class AngularAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/salimkamboh/yii2-angularjs';
+    public $js = [
+        'js/angular.js',
+        'js/angular-animate.js',
+        'js/angular-cookies.js',
+        'js/angular-loader.js',
+        'js/angular-mocks.js',
+        'js/angular-resource.js',
+        'js/angular-route.js',
+        'js/angular-sanitize.js',
+        'js/angular-scenario.js',
+        'js/angular-touch.js',
+    ];
+}
